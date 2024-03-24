b50115f99c3452158e7bac024e46cb4976c8f2f6
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-03-24 11:06:39 -0300

    initialize repository

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+app.use(ToastService)
+
+app.mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import { useToast } from 'primevue/usetoast'
+
+const toast = useToast()
+const username = ref('')
+const password = ref('')
+
+const login = () => {
+  if (username.value === 'admin' && password.value === 'admin') {
+    toast.add({ severity: 'success', summary: 'Success', detail: 'Login successful', life: 3000 })
+  } else {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Invalid credentials', life: 3000 })
+  }
+}
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div class="left-panel"></div>
+
+    <div class="right-panel">
+      <div class="login-container">
+        <div class="login-header">
+          <h1>Login</h1>
+        </div>
+        <div class="login-body">
+          <div class="p-fluid">
+            <div class="p-field">
+              <label for="username">Username</label>
+              <InputText id="username" v-model="username" />
+            </div>
+            <div class="p-field">
+              <label for="password">Password</label>
+              <InputText id="password" v-model="password" />
+            </div>
+          </div>
+        </div>
+        <div class="login-footer">
+          <Button label="Login" @click="login" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-wrapper {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+.left-panel {
+  background-color: #3b82f6;
+  width: 50vw;
+  height: 100vh;
+}
+.right-panel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50vw;
+  height: 100vh;
+}
+.login-footer {
+  margin-top: 1rem;
+}
+</style>
