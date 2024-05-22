1ca0673bdc42bb1f02d5c5fb9503f751bb6241c5
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-05-22 10:25:03 -0300

    test: add tests for base input

--- src/components/__tests__/AppInputText.spec.ts
+++ src/components/__tests__/AppInputText.spec.ts
@@ -16,19 +16,31 @@ describe('AppInputText', () => {
   })
 
   it('renders properly', async () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renders the label when provided', async () => {
     wrapper.setProps({ label: 'Name' })
     await wrapper.vm.$nextTick()
-    expect(wrapper.text()).toContain('Name')
+    const labelElement = wrapper.find('label')
+    expect(labelElement.exists()).toBe(true)
+    expect(labelElement.text()).toBe('Name')
+  })
+
+  it('does not render the label when not provided', async () => {
+    const labelElement = wrapper.find('label')
+    expect(labelElement.exists()).toBe(false)
   })
 
   it('renders the error message when input is invalid', async () => {
-    wrapper.setProps({ modelValue: '', required: true })
+    wrapper.setProps({ modelValue: '', label: 'Name', required: true })
     await wrapper.vm.$nextTick()
     wrapper.vm.isValid()
     await wrapper.vm.$nextTick()
     const errorMessageElement = wrapper.find('.p-error')
     expect(errorMessageElement.exists()).toBe(true)
     expect(errorMessageElement.text()).toBe('This field is required')
+    expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('does not render the error message when input is valid', async () => {
