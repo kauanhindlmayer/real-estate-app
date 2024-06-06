01c805af33b077bdc9931a6347d59505924185ff
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-06-05 21:49:41 -0300

    refactor: add authorization header interceptor to exclude certain routes

--- src/gateways/interceptors/addAuthorizationHeader.ts
+++ src/gateways/interceptors/addAuthorizationHeader.ts
@@ -3,9 +3,9 @@ import { useUserStore } from '@/stores/user'
 
 function addAuthorizationHeader(config: InternalAxiosRequestConfig) {
   const userStore = useUserStore()
-  const token = userStore.user?.accessToken
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+  const urlsExcludedForBearerHeader = ['/login', '/register']
+  if (urlsExcludedForBearerHeader.indexOf(config.url!) === -1) {
+    config.headers.Authorization = `Bearer ${userStore.user?.accessToken}`
   }
   return config
 }
